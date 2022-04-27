0f35a3af4c93e3cd8f23f7dbfc5ed6e0f6f8dbb6
HiddenError <78518846+HiddenError@users.noreply.github.com>
2022-04-27 10:56:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-# Name: [Your Name]
+# Name: Noah R
 # Date: 4/24/2022
 # Project: Countdown Timer using Github
-# File: countdownClass --> main.py
+# File: countdownTimer --> main.py
 #-----------------------------------------------------------------
+
+import time
+
+def timeCountdown(userTime):
+    while userTime:
+        min, secs = divmod(userTime, 60)
+        timer = "{:02d}:{:20d}".format(min, secs)
+        print("\r", timer, end=" ")
+        time.sleep(1)
+        userTime -= 1
+userTime = input("Please enter the time in seconds:\n")
+timeCountdown(int(userTime))
